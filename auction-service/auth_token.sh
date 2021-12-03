28d5d9d48385112242554a9ff0a3da06b512e0b4
Park-J-lab <jingpk1998@outlook.com>
2021-12-03 19:22:07 +0800

    implement microservices

---
+++ auction-service/auth_token.sh
@@ -0,0 +1,7 @@
+curl --location --request POST 'https://dev-sb4248r8.us.auth0.com/oauth/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'client_id=hu5EGHqLI4HKoSsQJP3Ko7t4gRKGUYk7' \
+--data-urlencode 'username=parkjing@qq.com' \
+--data-urlencode 'password=9019Test!' \
+--data-urlencode 'grant_type=password' \
+--data-urlencode 'scope=openid'
